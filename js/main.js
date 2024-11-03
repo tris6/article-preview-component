525061597480a4558ce9087f8ed67c0e3112930c
tris6 <tris.curtis@gmail.com>
2024-11-03 14:33:07 +1030

    article-preview-component

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function displayDiv() {
+    var element = document.getElementById('card__share-bar-square');
+    element.classList.toggle('display-prop');
+    
+    element = document.getElementById('card__share-bar-arrow');
+    element.classList.toggle('display-prop');
+}
